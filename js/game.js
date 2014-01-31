f1c2e315828e65815895c186d4b97b2f2b1289d8
Tuan <ldtuan87@yahoo.com>
2014-01-31 22:17:41 +0700

    fix Amazon Affiliate link below Chitika ad

--- js/game.js
+++ js/game.js
@@ -121,7 +121,7 @@ var Game = {
 		//s.src = '//cdn.chitika.net/getads.js';
 		//try { document.getElementsByTagName('head')[0].appendChild(s); } catch(e) { document.write(s.outerHTML); }
 
-		document.write('<div style="float:right"><div style="float:right" id="chitikaAdBlock-' + placement_id + '"></div><script type="text/javascript">' +
+		document.write('<div style="float:right"><div id="chitikaAdBlock-' + placement_id + '"></div><script type="text/javascript">' +
 'amazon_ad_tag = "starrogue-20"; amazon_ad_width = "160"; amazon_ad_height = "600";</script>' +
 '<script type="text/javascript" src="http://ir-na.amazon-adsystem.com/s/ads.js"></script></div>');
 		//document.write('<div style="float:right" id="chitikaAdBlock-' + placement_id + '"></div>');
